8bc6efeb980b12bede38047a525158f00343f6c7
jmindevelper <jmin.developer@gmail.com>
2021-08-21 21:05:17 +0900

    성공

--- 백준_2908_Wift/백준_2908_Wift/main.swift
+++ 백준_2908_Wift/백준_2908_Wift/main.swift
@@ -5,7 +5,22 @@
 //  Created by J_Min on 2021/08/21.
 //
 
+// https://www.acmicpc.net/problem/2908
+
 import Foundation
 
-print("Hello, World!")
+let input = readLine()!.split(separator: " ").map { Int(String($0.reversed()))! }
+
+print(max(input[0], input[1]))
+
+/*
+
+ max(_ x:_ y:)
+ 
+ x: A value to compare
+ y: Another value to compare
+ 
+ The greather of x and y. If x is equal to y, return y
+ 
+*/
 
---
+++ 백준_5622_Swift/백준_5622_Swift/main.swift
@@ -0,0 +1,39 @@
+//
+//  main.swift
+//  백준_5622_Swift
+//
+//  Created by J_Min on 2021/08/21.
+//
+
+// https://www.acmicpc.net/problem/5622
+
+import Foundation
+
+let input = readLine()!
+var count: Int = 0
+
+
+for i in input {
+    switch i {
+    case "A", "B", "C":
+        count += 3
+    case "D", "E", "F":
+        count += 4
+    case "G", "H", "I":
+        count += 5
+    case "J", "K", "L":
+        count += 6
+    case "M", "N", "O":
+        count += 7
+      case "P", "Q", "R", "S":
+        count += 8
+      case "T", "U", "V":
+        count += 9
+      case "W", "X", "Y", "Z":
+        count += 10
+    default:
+        break
+    }
+}
+
+print(count)
