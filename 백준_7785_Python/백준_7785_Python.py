a7de6279b0f0a772601b8ac0b3df717b3ae28b61
jmindevelper <jmin.developer@gmail.com>
2021-08-22 22:42:08 +0900

    성공

---
+++ 백준_7785_Python/백준_7785_Python.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+members = []
+
+for i in range(n):
+    name, record = input().split()
+
+    if record == "enter":
+        members.append(name)
+    else:
+        members.remove(name)
+
+for i in sorted(members, reverse=True):
+    print(i)
